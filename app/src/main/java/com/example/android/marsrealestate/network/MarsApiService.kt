903deebedd3dc670c1f22ca440bd413e23fb3682
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-05-30 17:42:45 +0700

    Filter the results

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -25,7 +25,13 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
+enum class MarsApiFilter(val value: String) {
+    SHOW_RENT("rent"),
+    SHOW_BUY("buy"),
+    SHOW_ALL("all")
+}
 private const val BASE_URL = " https://android-kotlin-fun-mars-server.appspot.com/"
 private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
@@ -38,7 +44,7 @@ private val retrofit = Retrofit.Builder()
 
 interface MarsApiService {
     @GET("realestate")
-    fun getProperties():
+    fun getProperties(@Query("filter") type: String):
             Deferred<List<MarsProperty>>
 }
 
