e7b3fd33903494b4f1ed1fd83b85078115a40910
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-05-30 15:43:09 +0700

    Display a grid of images with a RecyclerView

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -42,10 +42,10 @@ class OverviewViewModel : ViewModel() {
     val response: LiveData<String>
         get() = _response
 
-    private val _property = MutableLiveData<MarsProperty>()
+    private val _properties = MutableLiveData<List<MarsProperty>>()
 
-    val property: LiveData<MarsProperty>
-        get() = _property
+    val properties: LiveData<List<MarsProperty>>
+        get() = _properties
 
     private var viewModelJob = Job()
     private val coroutineScope = CoroutineScope(
@@ -68,9 +68,7 @@ class OverviewViewModel : ViewModel() {
             try {
                 var listResult = getPropertiesDeferred.await()
                 _response.value = "Success: ${listResult.size} Mars properties retrieved"
-                if (listResult.isNotEmpty()) {
-                    _property.value = listResult[0]
-                }
+                _properties.value = listResult
             } catch (e: Exception) {
                 _response.value = "Failure: ${e.message}"
             }
